67a44bab288e6255657bec759db44d11321d92f7
ryan-jones <ryanllewellynjones@gmail.com>
2023-03-03 09:47:11 +0100

    update server.py for pythonanywhere

--- server/server.py
+++ server/server.py
@@ -1,25 +1,28 @@
+from api import bp as api_bp
 from flask import Flask, render_template
 from flask_cors import CORS
+import os
 
-from config import Config
+from dotenv import load_dotenv
 
-
-def create_app(config_class=Config):
-    app = Flask(__name__, static_folder='../client/build/static',
-                template_folder='../client/build')
-    CORS(app)
-
-    app.config.from_object(config_class)
-
-    from api import bp as api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
-
-    @app.route('/')
-    def index():
-        return render_template("index.html", flask_token="hello world")
-
-    return app
+load_dotenv()
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-if __name__ == "__main__":
-    create_app()
+class Config:
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+app = Flask(__name__, static_folder='../client/build/static',
+            template_folder='../client/build')
+CORS(app)
+
+app.config.from_object(Config)
+
+app.register_blueprint(api_bp, url_prefix='/api')
+
+
+@app.route('/')
+def index():
+    return render_template("index.html", flask_token="hello world")
